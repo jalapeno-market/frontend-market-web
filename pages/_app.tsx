6c54e656bcab55f52b911dcd53eb06caca57d7be
mjmj0217 <mjmj0217@naver.com>
2022-11-14 21:15:57 +0900

    initialize project

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import Layout from "../components/layout/Layout";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
