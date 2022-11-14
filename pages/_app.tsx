bc65b2f0b04040f993447b89e1c2f8aa2e43a2ba
mjmj0217 <mjmj0217@naver.com>
2022-11-15 01:16:27 +0900

    feat: 공통 스타일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
