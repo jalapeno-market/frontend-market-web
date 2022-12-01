6e93259c3170cbd02e1307948b3cd59595ce23ab
mjmj0217 <mjmj0217@naver.com>
2022-12-02 00:33:29 +0900

    feat: 홈 화면으로 돌아가기 추가

--- components/layout/header/NewPostHeader.tsx
+++ components/layout/header/NewPostHeader.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "./NewPostHeader.module.scss";
+import { useRouter } from "next/router";
 
 const NewPostHeader = () => {
-  const clickCancelHandler = () => {};
-  const clickCompleteHandler = () => {};
+  const router = useRouter();
+  const clickCancelHandler = () => {
+    router.push("/home");
+  };
+  const clickCompleteHandler = () => {
+    //게시물 업로드
+  };
   return (
     <header className={styles["header-box"]}>
       <button onClick={clickCancelHandler} className={styles["cancel-btn"]}>
