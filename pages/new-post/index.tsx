1f4b999d50f40775cd909e7b020678079666fbce
mjmj0217 <mjmj0217@naver.com>
2023-01-08 11:14:28 +0900

    design: layout 수정에 따른 css 조정

--- pages/new-post/index.tsx
+++ pages/new-post/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Container from "../../components/common/Container";
 import Form from "../../components/post/Form";
 
 export default function PostPage() {
-  return (
-    <Container>
-      <Form />
-    </Container>
-  );
+  return <Form />;
 }
