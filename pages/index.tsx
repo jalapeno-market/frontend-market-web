f640ce37319cf7683ea873f8ce6bdbf504a8e746
mjmj0217 <mjmj0217@naver.com>
2023-01-10 15:10:33 +0900

    feat: 로그인, 회원가입 페이지 접근 제한

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { useLayoutEffect } from "react";
 import Logo from "../components/common/Logo";
 import Form from "../components/login/Form";
 
 export default function LoginPage() {
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (sessionStorage.getItem("id")) {
+      router.push("/home");
+    }
+  }, [router]);
+
   return (
     <>
       <Logo />
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+import { useLayoutEffect } from "react";
 import Logo from "../../components/common/Logo";
 import Form from "../../components/signup/Form";
 
 export default function SignUp() {
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (sessionStorage.getItem("id")) {
+      router.push("/home");
+    }
+  }, [router]);
   return (
     <>
       <Logo />
